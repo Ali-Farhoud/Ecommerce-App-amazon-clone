3dcc6ed6c9bf96625a732735b6438a33b4d44d69
Ali Farhoud <alifarhoud613@gmail.com>
2022-06-15 14:14:31 -0400

    updated styles

--- src/Home.js
+++ src/Home.js
@@ -9,18 +9,18 @@ function Home() {
             <img className='home__image' src="https://images-eu.ssl-images-amazon.com/images/G/31/prime/Gateway/2020/May/gaming_1500x600._CB431281464_.jpg" alt="" />
         </div>
         <div className="home__row">
-          <Product id="01"title="PlayStation 5 Console" price={449.99} image="https://m.media-amazon.com/images/I/51QKZfyi-dL._AC_UL320_.jpg" rating={5}/>
-          <Product id="02"title="Echo Dot (3rd Gen)" price={29.99} image="https://m.media-amazon.com/images/I/41CRnvYqmqL._DEALSCB_.jpg" rating={5}/>
+          <Product key ={1}id="01"title="PlayStation 5 Console" price={449.99} image="https://m.media-amazon.com/images/I/51QKZfyi-dL._AC_UL320_.jpg" rating={5}/>
+          <Product key ={2}id="02"title="Echo Dot (3rd Gen)" price={29.99} image="https://m.media-amazon.com/images/I/41CRnvYqmqL._DEALSCB_.jpg" rating={5}/>
 
         </div>
         <div className="home__row">
-          <Product id="03"title="The Lean Startup" price={19.99} image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" rating={5}/>
-          <Product id="04"title="Fifa 22 - Xbox One" price={49.99} image="https://m.media-amazon.com/images/I/81cAhbc2DzL._AC_UL320_.jpg" rating={5}/>
-          <Product id="05"title="George R.R. Martin's A Game Of Thrones: The Comic Book #19" price={4.39} image="https://m.media-amazon.com/images/I/918XEaCL7ZS._AC_UL320_.jpg" rating={5}/>
+          <Product key ={3} id="03"title="The Lean Startup" price={19.99} image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg" rating={5}/>
+          <Product key ={4}id="04"title="Fifa 22 - Xbox One" price={49.99} image="https://m.media-amazon.com/images/I/81cAhbc2DzL._AC_UL320_.jpg" rating={5}/>
+          <Product key ={5}id="05"title="George R.R. Martin's A Game Of Thrones: The Comic Book #19" price={4.39} image="https://m.media-amazon.com/images/I/918XEaCL7ZS._AC_UL320_.jpg" rating={5}/>
           
         </div>
         <div className="home__row">
-        <Product id="06"title="Samsung LS24R35AFHNXZA 24 LED-Lit Monitor 75Hz Freesync Dark Blue Grey" price={249.99} image="https://m.media-amazon.com/images/I/819Diw6CNJL._AC_UL320_.jpg" rating={4}/>
+        <Product key ={6}id="06"title="Samsung LS24R35AFHNXZA 24 LED-Lit Monitor 75Hz Freesync Dark Blue Grey" price={249.99} image="https://m.media-amazon.com/images/I/819Diw6CNJL._AC_UL320_.jpg" rating={4}/>
   
         </div>
     </div>
--- src/Product.js
+++ src/Product.js
@@ -10,6 +10,7 @@ function Product({id,title,price,image,rating}) {
     dispatch({
       type:"ADD_TO_BASKET",
       item:{
+        
         id,
         title,
         price,
